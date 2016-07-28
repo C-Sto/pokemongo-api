e82b6a268e97ea8af6b07413b146ce3396663fd9
C_Sto <cameronrstokes@outlook.com>
2016-07-29 06:36:45 +0800

    made to withstand shonky niantic servers a bit better

--- pogo/demo.py
+++ pogo/demo.py
@@ -406,7 +406,7 @@ def camBot(session):
             if bag[items.POKE_BALL] > 0 or bag[items.GREAT_BALL] > 0 or bag[items.ULTRA_BALL] > 0 or bag[items.MASTER_BALL] > 0:
                 coutn = 1
                 while True:
-                    coutn += 1
+                    coutn += 1  # lol at this being added now
                     if safe_catch(findNearPokemon(session), session, speed):
                         break
                     elif coutn >13:
